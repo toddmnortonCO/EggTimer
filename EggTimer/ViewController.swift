1313ff0481899e39bdad428f31ad0eab04a95b70
Todd Norton <toddmnorton@live.com>
2023-12-11 17:29:44 -0700

    timer countdown works!

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = ["Soft": 5, "Medium":7, "Hard": 12]
+    var eggCountdown = 0
+    let seconds = 60
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        let eggCountdown = result * seconds
         
-        if hardness == "Soft" {
-            print(softTime)
-        } else if hardness == "Medium" {
-            print(mediumTime)
-        } else {
-            print(hardTime)
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.eggCountdown > 0 {
+                print("\(self.eggCountdown) seconds remaining.")
+                self.eggCountdown -= 1
+            } else {
+                print("Eggs are done!")
+            }
         }
-    }
-    
-
+        
+        }
 }
+    
 
